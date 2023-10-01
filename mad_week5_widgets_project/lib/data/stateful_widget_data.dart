3a18190b0b4d1c984736c3db3d6a2c586c711365
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-10-01 01:21:45 -0700

    added some data for stateless and stateful widgets.

--- mad_week5_widgets_project/lib/data/stateful_widget_data.dart
+++ mad_week5_widgets_project/lib/data/stateful_widget_data.dart
@@ -8,10 +8,303 @@ class StateFulWidgetData {
 
 List<StateFulWidgetData> stateFulWidgetList = [
 
-  StateFulWidgetData('', '', ''''''),
-  StateFulWidgetData('', '', ''''''),
-  StateFulWidgetData('', '', ''''''),
-  StateFulWidgetData('', '', ''''''),
-  StateFulWidgetData('', '', ''''''),
+  StateFulWidgetData(
+      '',
+      '''
+      The checkbox itself does not maintain any state. Instead, when the state of the checkbox changes, the widget calls the onChanged callback. Most widgets that use a checkbox will listen for the onChanged callback and rebuild the checkbox with a new value to update the visual appearance of the checkbox.
+      ''',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Checkbox].
+
+void main() => runApp(const CheckboxExampleApp());
+
+class CheckboxExampleApp extends StatelessWidget {
+  const CheckboxExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Checkbox Sample')),
+        body: const Center(
+          child: CheckboxExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class CheckboxExample extends StatefulWidget {
+  const CheckboxExample({super.key});
+
+  @override
+  State<CheckboxExample> createState() => _CheckboxExampleState();
+}
+
+class _CheckboxExampleState extends State<CheckboxExample> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
+
+      '''
+  ),
+  StateFulWidgetData(
+      'Radio Widget',
+      '''
+      Used to select between a number of mutually exclusive values. When one radio button in a group is selected, the other radio buttons in the group cease to be selected. The values are of type T, the type parameter of the Radio class. Enums are commonly used for this purpose.
+      ''',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Radio].
+
+void main() => runApp(const RadioExampleApp());
+
+class RadioExampleApp extends StatelessWidget {
+  const RadioExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Radio Sample')),
+        body: const Center(
+          child: RadioExample(),
+        ),
+      ),
+    );
+  }
+}
+
+enum SingingCharacter { lafayette, jefferson }
+
+class RadioExample extends StatefulWidget {
+  const RadioExample({super.key});
+
+  @override
+  State<RadioExample> createState() => _RadioExampleState();
+}
+
+class _RadioExampleState extends State<RadioExample> {
+  SingingCharacter? _character = SingingCharacter.lafayette;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: const Text('Lafayette'),
+          leading: Radio<SingingCharacter>(
+            value: SingingCharacter.lafayette,
+            groupValue: _character,
+            onChanged: (SingingCharacter? value) {
+              setState(() {
+                _character = value;
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: const Text('Thomas Jefferson'),
+          leading: Radio<SingingCharacter>(
+            value: SingingCharacter.jefferson,
+            groupValue: _character,
+            onChanged: (SingingCharacter? value) {
+              setState(() {
+                _character = value;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+      '''
+  ),
+  StateFulWidgetData(
+      'Switch Widget',
+      '''
+      Used to toggle the on/off state of a single setting.
+
+The switch itself does not maintain any state. Instead, when the state of the switch changes, the widget calls the onChanged callback. Most widgets that use a switch will listen for the onChanged callback and rebuild the switch with a new value to update the visual appearance of the switch.
+      ''',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Switch].
+
+void main() => runApp(const SwitchApp());
+
+class SwitchApp extends StatelessWidget {
+  const SwitchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Switch Sample')),
+        body: const Center(
+          child: SwitchExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      // This bool value toggles the switch.
+      value: light,
+      activeColor: Colors.red,
+      onChanged: (bool value) {
+        // This is called when the user toggles the switch.
+        setState(() {
+          light = value;
+        });
+      },
+    );
+  }
+}
+
+      '''
+  ),
+  StateFulWidgetData(
+      'Slider Widget',
+      'Used to select from a range of values.',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Slider].
+
+void main() => runApp(const SliderApp());
+
+class SliderApp extends StatelessWidget {
+  const SliderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SliderExample(),
+    );
+  }
+}
+
+class SliderExample extends StatefulWidget {
+  const SliderExample({super.key});
+
+  @override
+  State<SliderExample> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<SliderExample> {
+  double _currentSliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Slider')),
+      body: Slider(
+        value: _currentSliderValue,
+        max: 100,
+        divisions: 5,
+        label: _currentSliderValue.round().toString(),
+        onChanged: (double value) {
+          setState(() {
+            _currentSliderValue = value;
+          });
+        },
+      ),
+    );
+  }
+}
+
+      '''
+  ),
+  StateFulWidgetData(
+      'TextField Widget',
+      'A text field lets the user enter text, either with hardware keyboard or with an onscreen keyboard.',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [TextField].
+
+class ObscuredTextFieldSample extends StatelessWidget {
+  const ObscuredTextFieldSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 250,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldExampleApp extends StatelessWidget {
+  const TextFieldExampleApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Obscured Textfield')),
+        body: const Center(
+          child: ObscuredTextFieldSample(),
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(const TextFieldExampleApp());
+
+      '''
+  ),
 
 ];
--- mad_week5_widgets_project/lib/data/stateless_widget_data.dart
+++ mad_week5_widgets_project/lib/data/stateless_widget_data.dart
@@ -50,4 +50,358 @@ List<StateLessWidgetData> stateLessWidgetList = [
   ),
 )
   """),
+  StateLessWidgetData(
+      'Text Widget',
+      'The Text widget displays a string of text with single style. The string might break across multiple lines or might all be displayed on the same line depending on the layout constraints.',
+      """
+      const Text.rich(
+  TextSpan(
+    text: 'Hello', // default text style
+    children: <TextSpan>[
+      TextSpan(text: ' beautiful ', style: TextStyle(fontStyle: FontStyle.italic)),
+      TextSpan(text: 'world', style: TextStyle(fontWeight: FontWeight.bold)),
+    ],
+  ),
+)
+  """),
+  StateLessWidgetData('Image Widget', 'A widget that displays an image.', """
+      const Image(
+  image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+)
+  """),
+  
+  StateLessWidgetData('Icon Widget', 'Icon Widget i used to display an Icon on Screen',
+      '''
+      const Row(
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+  children: <Widget>[
+    Icon(
+      Icons.favorite,
+      color: Colors.pink,
+      size: 24.0,
+      semanticLabel: 'Text to announce in accessibility modes',
+    ),
+    Icon(
+      Icons.audiotrack,
+      color: Colors.green,
+      size: 30.0,
+    ),
+    Icon(
+      Icons.beach_access,
+      color: Colors.blue,
+      size: 36.0,
+    ),
+  ],
+)
+  '''),
+
+  StateLessWidgetData(
+      'AppBar Widget',
+      'An app bar consists of a toolbar and potentially other widgets, such as a TabBar and a FlexibleSpaceBar. App bars typically expose one or more common actions with IconButtons which are optionally followed by a PopupMenuButton for less common operations (sometimes called the "overflow menu").',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [AppBar].
+
+void main() => runApp(const AppBarApp());
+
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AppBarExample(),
+    );
+  }
+}
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar Demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'This is the next page',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'This is the home page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+      '''
+  ),
+  StateLessWidgetData(
+      'Card Widget',
+      '''
+      A Material Design card: a panel with slightly rounded corners and an elevation shadow.
+      A card is a sheet of Material used to represent some related information, for example an album, a geographical location, a meal, contact details, etc.
+      This is what it looks like when run:
+      ''',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Card].
+
+void main() => runApp(const CardExampleApp());
+
+class CardExampleApp extends StatelessWidget {
+  const CardExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Card Sample')),
+        body: const CardExample(),
+      ),
+    );
+  }
+}
+
+class CardExample extends StatelessWidget {
+  const CardExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+      '''
+  ),
+  StateLessWidgetData(
+      'ListTile Widget',
+      'A single fixed-height row that typically contains some text as well as a leading or trailing icon.',
+      '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [ListTile].
+
+void main() => runApp(const ListTileApp());
+
+class ListTileApp extends StatelessWidget {
+  const ListTileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        listTileTheme: const ListTileThemeData(
+          textColor: Colors.white,
+        ),
+        useMaterial3: true,
+      ),
+      home: const ListTileExample(),
+    );
+  }
+}
+
+class ListTileExample extends StatefulWidget {
+  const ListTileExample({super.key});
+
+  @override
+  State<ListTileExample> createState() => _ListTileExampleState();
+}
+
+class _ListTileExampleState extends State<ListTileExample>
+    with TickerProviderStateMixin {
+  late final AnimationController _fadeController;
+  late final AnimationController _sizeController;
+  late final Animation<double> _fadeAnimation;
+  late final Animation<double> _sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _fadeController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _sizeController = AnimationController(
+      duration: const Duration(milliseconds: 850),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _fadeController,
+      curve: Curves.easeInOut,
+    );
+
+    _sizeAnimation = CurvedAnimation(
+      parent: _sizeController,
+      curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _fadeController.dispose();
+    _sizeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ListTile Samples')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Hero(
+            tag: 'ListTile-Hero',
+            // Wrap the ListTile in a Material widget so the ListTile has someplace
+            // to draw the animated colors during the hero transition.
+            child: Material(
+              child: ListTile(
+                title: const Text('ListTile with Hero'),
+                subtitle: const Text('Tap here for Hero transition'),
+                tileColor: Colors.cyan,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<Widget>(builder: (BuildContext context) {
+                      return Scaffold(
+                        appBar: AppBar(title: const Text('ListTile Hero')),
+                        body: Center(
+                          child: Hero(
+                            tag: 'ListTile-Hero',
+                            child: Material(
+                              child: ListTile(
+                                title: const Text('ListTile with Hero'),
+                                subtitle: const Text('Tap here to go back'),
+                                tileColor: Colors.blue[700],
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  );
+                },
+              ),
+            ),
+          ),
+          FadeTransition(
+            opacity: _fadeAnimation,
+            // Wrap the ListTile in a Material widget so the ListTile has someplace
+            // to draw the animated colors during the fade transition.
+            child: const Material(
+              child: ListTile(
+                title: Text('ListTile with FadeTransition'),
+                selectedTileColor: Colors.green,
+                selectedColor: Colors.white,
+                selected: true,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: Center(
+              child: SizeTransition(
+                sizeFactor: _sizeAnimation,
+                axisAlignment: -1.0,
+                // Wrap the ListTile in a Material widget so the ListTile has someplace
+                // to draw the animated colors during the size transition.
+                child: const Material(
+                  child: ListTile(
+                    title: Text('ListTile with SizeTransition'),
+                    tileColor: Colors.red,
+                    minVerticalPadding: 25.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+      '''),
+  StateLessWidgetData(
+      'Container Widget',
+      'A convenience widget that combines common painting, positioning, and sizing widgets.',
+      '''
+      Center(
+  child: Container(
+    margin: const EdgeInsets.all(10.0),
+    color: Colors.amber[600],
+    width: 48.0,
+    height: 48.0,
+  ),
+)
+      '''
+  ),
+  StateLessWidgetData('', '', ''''''),
 ];
