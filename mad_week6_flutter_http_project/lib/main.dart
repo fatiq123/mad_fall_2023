a4f9ac6df686434e9f3773a501af1a8d4762514e
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-10-05 21:49:39 -0700

    added crud functions for http api

--- mad_week6_flutter_http_project/lib/main.dart
+++ mad_week6_flutter_http_project/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+// get request
 Future<Album> fetchAlbum() async {
   final response = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
@@ -19,6 +20,59 @@ Future<Album> fetchAlbum() async {
   }
 }
 
+// post request
+Future<Album> createAlbum(String title, String body) async {
+  Map<String, dynamic> request = {
+    'title': title,
+    'body': body,
+    'userId': "111"
+  };
+
+  final uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+  final response = await http.post(uri, body: request);
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+// update api request
+Future<Album> updateAlbum(String title, String body) async {
+  Map<String, dynamic> request = {
+    'id': "101",
+    'title': title,
+    'body': body,
+    'userId': "111"
+  };
+
+  final uri = Uri.parse('https://jsonplaceholder.typicode.com/albums/1');
+  final response = await http.put(uri, body: request);
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+// delete api request
+Future<Album?>? deleteAlbum() async {
+  final uri = Uri.parse('https://jsonplaceholder.typicode.com/albums/1');
+  final response = await http.delete(uri);
+
+  if (response.statusCode == 200) {
+    return null;
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
 class Album {
   final int userId;
   final int id;
@@ -30,7 +84,6 @@ class Album {
     required this.title,
   });
 
-
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
       userId: json['userId'],
@@ -38,8 +91,6 @@ class Album {
       title: json['title'],
     );
   }
-
-
 }
 
 void main() => runApp(const MyApp());
@@ -52,15 +103,125 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
+  // late Future<Album> futureAlbum;
+  Future<Album?>? album;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    album = fetchAlbum();
+  }
+
+  void clickGetButton() {
+    setState(() {
+      album = fetchAlbum();
+    });
+  }
+
+  void clickDeleteButton() {
+    setState(() {
+      album = deleteAlbum();
+    });
+  }
+
+  void clickPostButton() {
+    setState(() {
+      album = createAlbum("top post", "This is an example of post");
+    });
+  }
+
+  void clickUpdateButton() {
+    setState(() {
+      album = updateAlbum("updated post", "This is an example of updated post");
+    });
   }
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Http Example"),
+        ),
+      ),
+      body: SizedBox(
+        height: 500,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FutureBuilder(
+              future: album,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.connectionState == ConnectionState.none) {
+                  return Container();
+                } else {
+                  if (snapshot.hasData) {
+                    return buildDataWidget(context, snapshot);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  } else {
+                    return Container();
+                  }
+                }
+              },
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () => clickPostButton(),
+                child: const Text("Post"),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () => clickGetButton(),
+                child: const Text("Get"),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () => clickUpdateButton(),
+                child: const Text("Update"),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () => clickDeleteButton(),
+                child: const Text("Delete"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDataWidget(context, snapshot) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Text(snapshot.data!.title),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(snapshot.data!.description),
+        )
+      ],
+    );
+  }
+
+/*@override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fetch Data Example',
@@ -88,5 +249,5 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
-  }
+  }*/
 }
